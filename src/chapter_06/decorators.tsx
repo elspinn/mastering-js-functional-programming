6063bc18a498262d34f6471af52987fb71f9a2d7
Elias Spinn <elias.spinn@workday.com>
2024-01-25 09:41:04 +0000

    memoization

--- src/chapter_06/decorators.tsx
+++ src/chapter_06/decorators.tsx
@@ -2,10 +2,18 @@ import {fibonacci} from "./fibonacci.tsx";
 
 
 const fibonacciWithLogger = withLogger(fibonacci);
-new Array(5).fill(0).forEach((_, i) => fibonacciWithLogger(i))
+// new Array(5).fill(0).forEach((_, i) => fibonacciWithLogger(i))
 
 const fibonacciWithPerformance = withPerformance(fibonacci);
-new Array(50).fill(0).forEach((_, i) => fibonacciWithPerformance(i))
+// new Array(10).fill(0).forEach((_, i) => fibonacciWithPerformance(i))
+// fibonacciWithPerformance(10)
+fibonacciWithPerformance(100)
+fibonacciWithPerformance(100)
+fibonacciWithPerformance(101)
+
+const fibonacciWithMemoAndPerf = withPerformance(memoize(fibonacci));
+// fibonacciWithMemoAndPerf(40)
+// fibonacciWithMemoAndPerf(40)
 
 //A decorator function that adds logging statements before and after a given function is called
 function withLogger<T extends (...args: any[]) => any>(fn: T) {
@@ -18,6 +26,7 @@ function withLogger<T extends (...args: any[]) => any>(fn: T) {
 }
 
 //A decorator function that adds performance measurements to a function;
+//Purer function with optional output and timer function parameters
 function withPerformance<T extends (...args: any[]) => any>(
     fn: T,
     output: (...results: string[]) => void = console.log.bind(console),
@@ -28,5 +37,13 @@ function withPerformance<T extends (...args: any[]) => any>(
         const returnValue = fn(...args);
         output(`${fn.name} execution time: ${timer() - startTime}`, `Function arguments: ${args}`)
         return returnValue;
+    }
+}
+
+//A memoizing function that caches function results;
+function memoize<T extends (n: number) => any>(fn: T) {
+    const cache: Record<number, ReturnType<T>> = {};
+    return (n: number): ReturnType<T> => {
+        return n in cache ? cache[n] : (cache[n] = fn(n))
     }
 }
--- src/chapter_06/fibonacci.tsx
+++ src/chapter_06/fibonacci.tsx
@@ -1,4 +1,27 @@
+//Memoized fibonacci
+const memoizedFibonacci = function() {
+    const cache: Record<number, number> = {};
+
+    return (n: number): number => {
+        if(n in cache){
+            console.log(`Cached result for ${n}`)
+            return cache[n]
+        } else if (n <= 0) {
+            console.log(`New result for ${n}`)
+            return 0
+        } else if (n == 1){
+            console.log(`New result for ${n}`)
+            return 1
+        }
+
+        console.log(`New result for ${n}`)
+        return cache[n] = fibonacci(n - 2) + fibonacci(n - 1)
+    }
+}();
+
+//Recursive fibonacci function
 function fibonacci(n: number): number {
+
     if (n <= 0) {
         return 0
     } else if (n == 1) {
@@ -8,4 +31,6 @@ function fibonacci(n: number): number {
     return fibonacci(n - 2) + fibonacci(n - 1)
 }
 
-export {fibonacci};
+
+
+export {fibonacci, memoizedFibonacci};
